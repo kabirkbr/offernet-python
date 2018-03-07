17431120d40a52c94fca3b365d54bf612f97a0df
kabir <kabir.33y@gmail.com>
2018-03-07 18:34:04 +0100

    manual index creation

--- offernet_dsl/on.py
+++ offernet_dsl/on.py
@@ -9,8 +9,10 @@
 from gremlin_python.driver.client import Client
 from gremlin_python.structure.graph import Graph
 from gremlin_python.driver.driver_remote_connection import DriverRemoteConnection
+from offernet_dsl.dsl import *
+import time
 
-
+VERTEX_TYPE='type'
 VERTEX_AGENT='agent'
 VERTEX_WORK='work'
 VERTEX_ITEM='item'
@@ -25,12 +27,61 @@ KEY_AGENT_ID='agentId'
 def init():
     client = Client('ws://localhost:8182/gremlin', 'g')
     rc = DriverRemoteConnection('ws://localhost:8182/gremlin', 'g')
-    g = Graph().traversal().withRemote(rc)
+    on = Graph().traversal(OfferNetTraversalSource).withRemote(rc)
 
-    if len(g.V().hasLabel('GlobalOfferNetProperties').toList()) is 0:
-        g.addV('GlobalOfferNetProperties').next()
-
-    schema_msg = """mgmt = graph.openManagement()
-                    string_prop = mgmt.makePropertyKey('agentId').dataType(String.class).cardinality(Cardinality.SINGLE).make()
+    make_keys_msg = f"""graph.tx().rollback()
+                    mgmt = graph.openManagement()
+                    
+                    // make propertyKeys                    
+                    mgmt.makePropertyKey('{VERTEX_TYPE}').dataType(String.class).cardinality(Cardinality.SINGLE).make()
+                    mgmt.makePropertyKey('{KEY_AGENT_ID}').dataType(String.class).cardinality(Cardinality.SINGLE).make()
+                    mgmt.makePropertyKey('{EDGE_OWNS}').dataType(String.class).cardinality(Cardinality.SINGLE).make()
+                    mgmt.makePropertyKey('{EDGE_SIMILAR}').dataType(String.class).cardinality(Cardinality.SINGLE).make()
+                    mgmt.makePropertyKey('{EDGE_OFFERS}').dataType(String.class).cardinality(Cardinality.SINGLE).make()
+                    mgmt.makePropertyKey('{EDGE_DEMANDS}').dataType(String.class).cardinality(Cardinality.SINGLE).make()
                     mgmt.commit()"""
+
+    schema_msg = f"""graph.tx().rollback()
+                    mgmt = graph.openManagement()
+                    
+                    vertexType = mgmt.getPropertyKey('{VERTEX_TYPE}')
+                    mgmt.buildIndex('byVertexTypeComposite', Vertex.class).addKey(vertexType).buildCompositeIndex()
+                    
+                    keyAgentId = mgmt.getPropertyKey('{KEY_AGENT_ID}')
+                    mgmt.buildIndex('byKeyAgentIdComposite', Vertex.class).addKey(keyAgentId).buildCompositeIndex()
+                    
+                    edgeOwns = mgmt.getPropertyKey('{EDGE_OWNS}')
+                    mgmt.buildIndex('byEdgeOwnsComposite', Edge.class).addKey(edgeOwns).buildCompositeIndex()
+                    
+                    edgeSimilar = mgmt.getPropertyKey('{EDGE_SIMILAR}')
+                    mgmt.buildIndex('byEdgeSimilarComposite', Edge.class).addKey(edgeSimilar).buildCompositeIndex()
+                    
+                    edgeOffers = mgmt.getPropertyKey('{EDGE_OFFERS}')
+                    mgmt.buildIndex('byEdgeOffersComposite', Edge.class).addKey(edgeOffers).buildCompositeIndex()
+                    
+                    edgeDemands = mgmt.getPropertyKey('{EDGE_DEMANDS}')
+                    mgmt.buildIndex('byEdgeDemandsComposite', Edge.class).addKey(edgeDemands).buildCompositeIndex()
+                    
+                    mgmt.commit()
+                    
+                    mgmt.awaitGraphIndexStatus(graph, 'byVertexTypeComposite').call()
+                    mgmt.awaitGraphIndexStatus(graph, 'byKeyAgentIdComposite').call()
+                    mgmt.awaitGraphIndexStatus(graph, 'byEdgeOwnsComposite').call()
+                    mgmt.awaitGraphIndexStatus(graph, 'byEdgeSimilarComposite').call()
+                    mgmt.awaitGraphIndexStatus(graph, 'byEdgeOffersComposite').call()
+                    mgmt.awaitGraphIndexStatus(graph, 'byEdgeDemandsComposite').call()
+                    
+                    mgmt = graph.openManagement()
+                    mgmt.updateIndex(mgmt.getGraphIndex("byVertexTypeComposite"), SchemaAction.REINDEX).get()
+                    mgmt.updateIndex(mgmt.getGraphIndex("byKeyAgentIdComposite"), SchemaAction.REINDEX).get()
+                    mgmt.updateIndex(mgmt.getGraphIndex("byEdgeOwnsComposite"), SchemaAction.REINDEX).get()
+                    mgmt.updateIndex(mgmt.getGraphIndex("byEdgeSimilarComposite"), SchemaAction.REINDEX).get()
+                    mgmt.updateIndex(mgmt.getGraphIndex("byEdgeOffersComposite"), SchemaAction.REINDEX).get()
+                    mgmt.updateIndex(mgmt.getGraphIndex("byEdgeDemandsComposite"), SchemaAction.REINDEX).get()
+                    mgmt.commit()"""
+    client.submit(make_keys_msg)
+    time.sleep(2)
     client.submit(schema_msg)
+
+    if len(on.V().has('type', 'GlobalOfferNetProperties').toList()) is 0:
+        on.addV('GlobalOfferNetProperties').property('type', 'GlobalOfferNetProperties').next()

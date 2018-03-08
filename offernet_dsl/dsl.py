1ea29832739ef82cb93b4e6acb81473583c4adea
kabir <kabir.33y@gmail.com>
2018-03-08 19:48:50 +0100

    bunch of methods and tests for offernet dsl

--- offernet_dsl/dsl.py
+++ offernet_dsl/dsl.py
@@ -11,6 +11,7 @@ from gremlin_python.process.graph_traversal import (GraphTraversalSource, GraphT
 from gremlin_python.process.graph_traversal import __ as AnonymousTraversal
 import uuid
 from offernet_dsl.on import *
+from offernet_dsl.utils import *
 
 V = AnonymousTraversal.V
 addV = AnonymousTraversal.addV
@@ -32,12 +33,32 @@ class OfferNetTraversal(GraphTraversal):
     """The OfferNet Traversal class which exposes the available steps of the DSL."""
 
     def knows(self, to_agent):
-        #to_agent = self.get_graph_traversal().V().has(KEY_AGENT_ID, to_agentId)
-        #print('to_agent: ', to_agent)
-        traversal = self.addE('knows').to(to_agent)
+        """Tells that this agent knows another agent"""
+
+        traversal = self.addE(EDGE_KNOWS).to(to_agent)
         return traversal
 
-    # finished here...
+    def owns_work(self, work):
+        """This agent owns work"""
+        traversal = self.addE(EDGE_OWNS).to(work)
+        return traversal
+
+    def all_works(self):
+        """Retrieves all works of an agent"""
+        traversal = self.out(EDGE_OWNS)
+        return traversal
+
+    def demands(self, item):
+        """Adds existing item to a work vertex as a demand"""
+        traversal = self.addE(EDGE_DEMANDS).to(item)
+        return traversal
+
+    def offers(self, item):
+        """Adds existing item to a work vertex as an offer"""
+
+        traversal = self.addE(EDGE_OFFERS).to(item)
+        return traversal
+
 
 class __(AnonymousTraversal):
     """Spawns anonymous OfferNetTraversal instances for the DSL."""
@@ -68,3 +89,21 @@ class OfferNetTraversalSource(GraphTraversalSource):
         """Gets an agent by the id (not sure this is needed in decentralized system) !!!"""
         traversal = self.get_graph_traversal().V().has(KEY_AGENT_ID, agent_id)
         return traversal
+
+    def create_work(self):
+        """Creates new work and returns it"""
+        traversal = self.get_graph_traversal().addV(VERTEX_WORK).property(VERTEX_TYPE, VERTEX_WORK).property(
+            KEY_WORK_ID, str(uuid.uuid4()))
+        return traversal
+
+    def create_item(self, *value):
+        """Creates new item and returns it"""
+        if len(value) == 0:
+            vector = binary_array(ITEM_VECTOR_LENGTH)
+        else:
+            vector = value[0]
+
+        traversal = self.get_graph_traversal().addV(VERTEX_ITEM).property(VERTEX_TYPE, VERTEX_ITEM).property(KEY_ITEM_ID, str(uuid.uuid4())).property(KEY_ITEM_VALUE, vector)
+
+        return traversal
+
--- tests/test_dsl.py
+++ tests/test_dsl.py
@@ -40,3 +40,52 @@ def test_agent():
     a2 = on.agent(a1).properties(KEY_AGENT_ID).value().next()
     assert a1 == a2
 
+def test_create_work():
+    w1 = on.create_work().properties(VERTEX_TYPE).value().next()
+    print('created work: ', w1)
+    assert w1 == VERTEX_WORK
+
+def test_create_random_item():
+    i1 = on.create_item().properties(VERTEX_TYPE).value().next()
+    print('created work: ', i1)
+    assert i1 == VERTEX_ITEM
+
+def test_create_nonrandom_item():
+    value = binary_array(ITEM_VECTOR_LENGTH)
+    i1 = on.create_item(value).properties(VERTEX_TYPE).value().next()
+    print('created work: ', i1)
+    assert i1 == VERTEX_ITEM
+
+def test_owns_work():
+    a1 = on.create_agent()
+    w1 = on.create_work()
+    ownsEdge = a1.owns_work(w1)
+    print('Created owns edge: ', ownsEdge)
+    assert ownsEdge.label().next() == EDGE_OWNS
+
+def test_all_works():
+    a1 = on.create_agent().properties(KEY_AGENT_ID).value().next()
+    w1 = on.create_work()
+    w2 = on.create_work()
+    on.agent(a1).owns_work(w1).next()
+    on.agent(a1).owns_work(w2).next()
+
+    works = on.agent(a1).all_works().properties(VERTEX_TYPE).value().toList()
+    assert len(works) == 2
+    for work in works:
+        assert work == VERTEX_WORK
+
+
+def test_demands():
+    w1 = on.create_work()
+    i1 = on.create_item()
+    demandsEdge = w1.demands(i1)
+    print('Created demands edge: ', demandsEdge)
+    assert demandsEdge.label().next() == EDGE_DEMANDS
+
+def test_offers():
+    w1 = on.create_work()
+    i1 = on.create_item()
+    offersEdge = w1.offers(i1)
+    print('Created offers edge: ', offersEdge)
+    assert offersEdge.label().next() == EDGE_OFFERS
